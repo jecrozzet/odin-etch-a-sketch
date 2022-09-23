116eafc9b92fb2a9e5c3e9a3bf5e292df5ab8a71
jecrozzet <jecrozzet@gmail.com>
2022-09-23 20:21:53 +0800

    Add feature change grid size

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function createGrid() {
+function createGrid(gridSize) {
     const gridContainer = document.createElement("div");
     gridContainer.classList.add("grid-container");
 
-    for (let row = 0; row < 16; row++) {
+    for (let row = 0; row < gridSize; row++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
 
-        for (let col = 0; col <16; col++) {
+        for (let col = 0; col < gridSize; col++) {
             const gridCol = document.createElement("div");
             gridCol.classList.add("grid-col");
             gridCol.addEventListener("mouseover", function (e) {
@@ -23,4 +23,36 @@ function createGrid() {
     document.body.appendChild(gridContainer);
 }
 
-createGrid();
+function removeGrid() {
+    const gridContainer = document.querySelector(".grid-container");
+    document.body.removeChild(gridContainer);
+}
+
+function getGridSize() {
+    const gridSize = Number(prompt("Input Grid Size (1 - 100)", "16"));
+
+    if (isNaN(gridSize)) {
+        alert("Please Input a Number!");
+        return;
+    }
+
+    if (gridSize < 1) {
+        gridSize = 1;
+    } else if (gridSize > 100) {
+        gridSize = 100;
+    }
+
+    return gridSize;
+}
+
+createGrid(16);
+
+const changeButton = document.querySelector("#change-button");
+changeButton.addEventListener("click", function () {
+    const gridSize = getGridSize();
+
+    if (gridSize) {
+        removeGrid();
+        createGrid(gridSize);
+    }
+});
