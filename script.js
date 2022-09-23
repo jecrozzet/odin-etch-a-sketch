b8a89c53a622f839f1d7874410688520fcfe6888
jecrozzet <jecrozzet@gmail.com>
2022-09-23 19:02:21 +0800

    Add createGrid with styling
    appendChild returns error using previous index.html version because the
    js file is loaded before html get fully loaded. Fixed by adding defer
    tag into script reference.

---
+++ script.js
@@ -0,0 +1,22 @@
+function createGrid() {
+    const gridContainer = document.createElement("div");
+    gridContainer.classList.add("grid-container");
+
+    for (let row = 0; row < 16; row++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+
+        for (let col = 0; col <16; col++) {
+            const gridCol = document.createElement("div");
+            gridCol.classList.add("grid-col");
+
+            gridRow.appendChild(gridCol);
+        }
+
+        gridContainer.appendChild(gridRow);
+    }
+
+    document.body.appendChild(gridContainer);
+}
+
+createGrid();
