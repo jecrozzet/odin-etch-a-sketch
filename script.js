0abe1cf3b59414aa5e77a20a857e84b747ed6553
jecrozzet <jecrozzet@gmail.com>
2022-09-24 15:01:26 +0800

    Add mousedown for painting and other features
    Disabled drag and drop on body element because it interrupts painting.
    Other features are grid lines toggle and clear button.

--- script.js
+++ script.js
@@ -11,11 +11,19 @@ function createGrid(gridSize) {
             const gridCol = document.createElement("div");
             gridCol.style.width = String((containerSize / gridSize) - 2) + "px";
             gridCol.style.height = String((containerSize / gridSize) - 2) + "px";
-            gridCol.style.border = "1px solid gray";
+            gridCol.classList.add("grid-col", "grid-on");
 
             gridCol.addEventListener("mouseover", function (e) {
-                const gridHover = e.target;
-                gridHover.classList.add("hover");
+                if(e.buttons === 1) {
+                    const gridOver = e.target;
+                    gridOver.classList.add("painted");
+                }
+            });
+            gridCol.addEventListener("mousedown", function (e) {
+                if(e.buttons === 1) {
+                    const gridOver = e.target;
+                    gridOver.classList.add("painted");
+                }
             });
 
             gridRow.appendChild(gridCol);
@@ -49,14 +57,32 @@ function getGridSize() {
     return gridSize;
 }
 
-createGrid(16);
+let gridSize = 16;
+createGrid(gridSize);
 
 const changeButton = document.querySelector("#change-button");
 changeButton.addEventListener("click", function () {
-    const gridSize = getGridSize();
+    gridSize = getGridSize();
 
     if (gridSize) {
         removeGrid();
         createGrid(gridSize);
     }
+});
+
+const gridLineButton = document.querySelector("#grid-line-button");
+gridLineButton.addEventListener("click", function () {
+    const gridCols = document.querySelectorAll(".grid-col");
+    gridCols.forEach(gridCol => {
+        gridCol.classList.toggle("grid-on");
+        gridCol.classList.toggle("grid-off");
+    });
+});
+
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", function () {
+    const gridCols = document.querySelectorAll(".grid-col");
+    gridCols.forEach(gridCol => { 
+        gridCol.classList.remove("painted");
+    });
 });
